0e6908b547e13fc212b8c563c5824d475bb132c8
serimi <rhdideks@gmail.com>
2023-05-15 15:08:24 +0900

    0515

--- app/src/main/java/com/example/registerlogin/FingerprintActivity.java
+++ app/src/main/java/com/example/registerlogin/FingerprintActivity.java
@@ -6,141 +6,126 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.biometrics.BiometricPrompt;
-import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CancellationSignal;
 import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyPermanentlyInvalidatedException;
 import android.security.keystore.KeyProperties;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-
 
 public class FingerprintActivity extends AppCompatActivity {
 
-    // create a CancellationSignal
-    // variable and assign a
-    // value null to it
     private CancellationSignal cancellationSignal = null;
-
-    // create an authenticationCallback
     private BiometricPrompt.AuthenticationCallback authenticationCallback;
     private Button start_authentication;
 
+    private static final String KEY_NAME = "my_key";
+    private KeyStore keyStore;
+
     @TargetApi(Build.VERSION_CODES.P)
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fingerprint);
 
         authenticationCallback = new BiometricPrompt.AuthenticationCallback() {
-            // here we need to implement two methods
-            // onAuthenticationError and
-            // onAuthenticationSucceeded If the
-            // fingerprint is not recognized by the
-            // app it will call onAuthenticationError
-            // and show a toast
+
             @Override
-            public void onAuthenticationError(
-                    int errorCode, CharSequence errString)
-            {
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
                 super.onAuthenticationError(errorCode, errString);
-                notifyUser("Authentication Error : " + errString);
+                notifyUser("Authentication Error: " + errString);
             }
-            // If the fingerprint is recognized by the
-            // app then it will call
-            // onAuthenticationSucceeded and show a
-            // toast that Authentication has Succeed
-            // Here you can also start a new activity
-            // after that
+
             @Override
-            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result)
-            {
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
                 notifyUser("Authentication Succeeded");
-                // or start a new Activity
+                // 처리를 진행하세요
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                notifyUser("Authentication Failed");
             }
         };
 
-        checkBiometricSupport();
-        // create a biometric dialog on Click of button
-
-        start_authentication=findViewById(R.id.start_authentication);
+        start_authentication = findViewById(R.id.start_authentication);
         start_authentication.setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.P)
             @Override
-            public void onClick(View view)
-            {
-                // This creates a dialog of biometric
-                // auth and it requires title , subtitle
-                // , and description In our case there
-                // is a cancel button by clicking it, it
-                // will cancel the process of
-                // fingerprint authentication
-                BiometricPrompt biometricPrompt = new BiometricPrompt
-                        .Builder(getApplicationContext())
-                        .setTitle("Title of Prompt")
-                        .setSubtitle("Subtitle")
-                        .setDescription("Uses FP")
-                        .setNegativeButton("Cancel", getMainExecutor(), new DialogInterface.OnClickListener() {
-                            @Override
-                            public void
-                            onClick(DialogInterface dialogInterface, int i)
-                            {
-                                notifyUser("Authentication Cancelled");
-                            }
-                        }).build();
+            public void onClick(View view) {
+                if (checkBiometricSupport()) {
+                    BiometricPrompt biometricPrompt = new BiometricPrompt.Builder(FingerprintActivity.this)
+                            .setTitle("Title of Prompt")
+                            .setSubtitle("Subtitle")
+                            .setDescription("Uses FP")
+                            .setNegativeButton("Cancel", getMainExecutor(), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    notifyUser("Authentication Cancelled");
+                                }
+                            }).build();
 
-                // start the authenticationCallback in
-                // mainExecutor
-                biometricPrompt.authenticate(
-                        getCancellationSignal(),
-                        getMainExecutor(),
-                        authenticationCallback);
+                    biometricPrompt.authenticate(getCancellationSignal(), getMainExecutor(), authenticationCallback);
+                }
             }
         });
+
+        generateKeyPair();
     }
 
-    // it will be called when
-    // authentication is cancelled by
-    // the user
-    private CancellationSignal getCancellationSignal()
-    {
+    private void generateKeyPair() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+
+            // Generate the key pair if it doesn't exist
+            if (!keyStore.containsAlias(KEY_NAME)) {
+                KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+                keyPairGenerator.initialize(
+                        new KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+                                .setUserAuthenticationRequired(true)
+                                .build()
+                );
+                KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            }
+
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidAlgorithmParameterException
+                 | CertificateException | IOException | KeyStoreException e) {
+            e.printStackTrace();
+            // 예외 처리를 수행하거나 오류 메시지를 표시하는 것이 좋습니다.
+        }
+    }
+
+    private CancellationSignal getCancellationSignal() {
         cancellationSignal = new CancellationSignal();
-        cancellationSignal.setOnCancelListener(
-                new CancellationSignal.OnCancelListener() {
-                    @Override public void onCancel()
-                    {
-                        notifyUser("Authentication was Cancelled by the user");
-                    }
-                });
+        cancellationSignal.setOnCancelListener(() -> notifyUser("Authentication was Cancelled by the user"));
         return cancellationSignal;
     }
 
-    // it checks whether the
-    // app the app has fingerprint
-    // permission
     @TargetApi(Build.VERSION_CODES.P)
     private Boolean checkBiometricSupport() {
         KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
@@ -153,78 +138,16 @@ public class FingerprintActivity extends AppCompatActivity {
             return false;
         }
         if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
+            generateKeyPair(); // 키 쌍 생성
             return true;
-        } else
-            return true;
-
-        generateKey();
-        if (cipherInit()) {
-            cryptoObject = new FingerprintManager.CryptoObject(cipher);
-            FingerprintHandler fingerprintHandler = new FingerprintHandler(this);
-            fingerprintHandler.startAutho(fingerprintManager, cryptoObject);
-        }
-    }
-
-        public boolean cipherInit(){
-        try {
-            cipher = Cipher.getInstance(
-                    KeyProperties.KEY_ALGORITHM_AES + "/"
-                            + KeyProperties.BLOCK_MODE_CBC + "/"
-                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
-        } catch (NoSuchAlgorithmException |
-                 NoSuchPaddingException e) {
-            throw new RuntimeException("Failed to get Cipher", e);
-        }
-        try {
-            keyStore.load(null);
-            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
-                    null);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            return true;
-        } catch (KeyPermanentlyInvalidatedException e) {
+        } else {
             return false;
-        } catch (KeyStoreException | CertificateException
-                 | UnrecoverableKeyException | IOException
-                 | NoSuchAlgorithmException | InvalidKeyException e) {
-            throw new RuntimeException("Failed to init Cipher", e);
         }
     }
 
-        //Key Generator
-        protected void generateKey() {
-        try {
-            keyStore = KeyStore.getInstance("AndroidKeyStore");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
-        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
-            throw new RuntimeException("Failed to get KeyGenerator instance", e);
-        }
-
-        try {
-            keyStore.load(null);
-            keyGenerator.init(new KeyGenParameterSpec.Builder(KEY_NAME,
-                    KeyProperties.PURPOSE_ENCRYPT |
-                            KeyProperties.PURPOSE_DECRYPT)
-                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-                    .setUserAuthenticationRequired(true)
-                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-                    .build());
-            keyGenerator.generateKey();
-        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | CertificateException | IOException e){
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    private class
-
-    // this is a toast method which is responsible for
-    // showing toast it takes a string as parameter
-    private void notifyUser(String message)
-    {
+    private void notifyUser(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
+
+
